1fd3843d1027da4b0e497e71efd20fa74d67ccdc
Aspen Hollyer <aspen.dollins@gmail.com>
2018-09-16 15:10:56 -0500

    Add basic sidenav

--- casn-frontend/casn-app/src/app/app.module.ts
+++ casn-frontend/casn-app/src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+/* Angular Material Components */
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatSidenavModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
