4b38f806cb40655976d57417a836095a9723e942
Aspen Hollyer <aspen.dollins@gmail.com>
2018-09-16 15:19:48 -0500

    Add menu icon to toggle sidenav

--- casn-frontend/casn-app/src/app/app.module.ts
+++ casn-frontend/casn-app/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 /* Angular Material Components */
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     BrowserAnimationsModule,
     MatSidenavModule,
     MatToolbarModule,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
