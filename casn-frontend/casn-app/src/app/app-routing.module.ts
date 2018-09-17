e4b256c430ebc0c81ccf66a74bc1c11eb1503f87
Aspen Hollyer <aspen.dollins@gmail.com>
2018-09-17 17:28:48 -0500

    Set patients route as first step to schedule a ride

--- casn-frontend/casn-app/src/app/app-routing.module.ts
+++ casn-frontend/casn-app/src/app/app-routing.module.ts
@@ -3,11 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 // import { AuthGuard } from './auth-guard.service';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { RidesComponent } from './rides/rides.component';
+import { PatientsComponent } from './patients/patients.component'
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
-  { path: 'schedule-ride', component: RidesComponent },
+  { path: 'patient', component: PatientsComponent },
   // { path: '', redirectTo: '/dashboard', pathMatch: 'full', canActivate: [ AuthGuard ] },
   // { path: 'dashboard', component: DashboardComponent, canActivate: [ AuthGuard ] },
   // { path: 'facts', component: FactsComponent, canActivate: [ AuthGuard ] },
