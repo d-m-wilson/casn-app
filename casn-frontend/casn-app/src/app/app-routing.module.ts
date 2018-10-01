c94b30c4bcca8429ac5bc8ab166a3e76aacc6c38
Aspen Hollyer <aspen.dollins@gmail.com>
2018-10-01 15:40:39 -0500

    Add frontend auth services

--- casn-frontend/casn-app/src/app/app-routing.module.ts
+++ casn-frontend/casn-app/src/app/app-routing.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-// import { AuthGuard } from './auth-guard.service';
+import { AuthGuard } from './auth-services/auth-guard.service';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { RidesComponent } from './rides/rides.component';
 import { PatientsComponent } from './patients/patients.component'
 
 const routes: Routes = [
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-  { path: 'dashboard', component: DashboardComponent },
-  { path: 'patient', component: PatientsComponent },
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full', canActivate: [ AuthGuard ] },
+  { path: 'dashboard', component: DashboardComponent, canActivate: [ AuthGuard ] },
+  { path: 'patient', component: PatientsComponent, canActivate: [ AuthGuard ] },
   // { path: '', redirectTo: '/dashboard', pathMatch: 'full', canActivate: [ AuthGuard ] },
   // { path: 'dashboard', component: DashboardComponent, canActivate: [ AuthGuard ] },
   // { path: 'facts', component: FactsComponent, canActivate: [ AuthGuard ] },
--- casn-frontend/casn-app/src/app/app.module.ts
+++ casn-frontend/casn-app/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -23,6 +24,13 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { AppRoutingModule } from './app-routing.module';
 import { PatientsComponent } from './patients/patients.component';
 import { RidesComponent } from './rides/rides.component';
+/* Custom Services & HTTP Interceptors */
+import { AuthGuard } from './auth-services/auth-guard.service';
+import { AuthenticationService } from './auth-services/auth.service';
+import { fakeBackendProvider } from './auth-services/fake-backend';
+import { JwtInterceptor } from './auth-services/jwt.interceptor';
+import { ErrorInterceptor } from './auth-services/error.interceptor';
+import { UserService } from './auth-services/user.service';
 
 @NgModule({
   declarations: [
@@ -48,7 +56,15 @@ import { RidesComponent } from './rides/rides.component';
     MatSidenavModule,
     MatToolbarModule,
   ],
-  providers: [],
+  providers: [
+    AuthGuard,
+    AuthenticationService,
+    UserService,
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+    // provider used to create fake backend for dev TODO: DELETE
+    // fakeBackendProvider
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
