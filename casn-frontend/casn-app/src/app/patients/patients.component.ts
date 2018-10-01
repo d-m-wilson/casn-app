9429dd923ef2d880c6a3ff11d1ca3dca64b4ad58
Aspen Hollyer <aspen.dollins@gmail.com>
2018-10-01 11:37:37 -0500

    add basic ui components for patient form

--- casn-frontend/casn-app/src/app/patients/patients.component.ts
+++ casn-frontend/casn-app/src/app/patients/patients.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import {Observable} from 'rxjs';
+import {map, startWith} from 'rxjs/operators';
 
 @Component({
   selector: 'app-patients',
@@ -6,10 +9,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./patients.component.css']
 })
 export class PatientsComponent implements OnInit {
+  languages = ['english', 'spanish', 'vietnamese'];
+  contactMethods = ['phone call', 'text', 'email'];
+
+  // PatientId - last 4 digits of phone number, add 5th digit on duplicate
+  patientForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    phoneNumber: new FormControl(''),
+    preferredLanguage: new FormControl(''),
+    preferredContactMethod: new FormControl(''),
+  });
+
+  patientSearchControl = new FormControl();
+  patientOptions: string[] = ['One', 'Two', 'Three'];
+  filteredPatientOptions: Observable<string[]>;
 
   constructor() { }
 
   ngOnInit() {
+    this.filteredPatientOptions = this.patientSearchControl.valueChanges
+      .pipe(
+        startWith(''),
+        map(value => this._filter(value))
+      );
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+    return this.patientOptions.filter(option => option.toLowerCase().includes(filterValue));
   }
 
 }
