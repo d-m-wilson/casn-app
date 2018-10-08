2c9e19600cae77da0dc241e5e57bbefd5b119d74
Aspen Hollyer <aspen.dollins@gmail.com>
2018-10-08 10:00:08 -0500

    Finish basic patient POST functionality

--- casn-frontend/casn-app/src/app/patients/patients.component.ts
+++ casn-frontend/casn-app/src/app/patients/patients.component.ts
@@ -12,7 +12,7 @@ import { Location } from '@angular/common';
   styleUrls: ['./patients.component.css']
 })
 export class PatientsComponent implements OnInit {
-  patient: any;
+  existingPatient: any = {};
   /* Display flags for patient lookup feature */
   displayPatientFoundModal: boolean = false;
   displayPatientForm: boolean = false;
@@ -29,8 +29,11 @@ export class PatientsComponent implements OnInit {
   /*********************************************************************
                                 Form
   **********************************************************************/
-  languages: string[] = ['English', 'Spanish', 'Other'];
-  contactMethods: string[] = ['Call', 'Text', 'Email'];
+  languages: string[] = ['English', 'Spanish', 'French', 'Other'];
+  // NOTE: Ask about number mapping here.
+  contactMethods: any[] = [ {value: 1, displayValue: 'Call'},
+                            {value: 2, displayValue: 'Text'},
+                            {value: 3, displayValue:'Email'} ];
 
   patientForm = this.fb.group({
     patientIdentifier: ['', [Validators.required, Validators.minLength(4),
@@ -43,23 +46,37 @@ export class PatientsComponent implements OnInit {
     preferredContactMethod: ['', Validators.required],
   })
 
-  onSubmit(): void {}
+  // convenience getter for easy access to form fields
+  get f() { return this.patientForm.controls; }
+
+  onSubmit(): void {
+    if(!this.patientForm.valid) { return; }
+    // Check if patient is new or existing to make appropriate REST call.
+    const isNewPatient = Object.keys(this.existingPatient).length === 0;
+    if(isNewPatient) {
+      this.saveNewPatient();
+    } else {
+      // TODO: There should be an update patient endpoint
+      this.saveNewPatient();
+      // this.updatePatient();
+    }
+  }
 
   /*********************************************************************
                               Click Handlers
   **********************************************************************/
-  handleYesClick() {
+  handleYesClick(): void {
     this.displayPatientFoundModal = false;
     this.displayPatientForm = true;
-    this.patientForm.setValue(this.patient);
+    this.patientForm.setValue(this.existingPatient);
   }
 
-  handleNoClick() {
+  handleNoClick(): void {
     this.displayPatientFoundModal = false;
-    this.patient = {}
+    this.existingPatient = {}
   }
 
-  handleCancelClick() {
+  handleCancelClick(): void {
     if(confirm('Are you sure? Any unsaved changes will be lost.')) {
       this.patientForm.reset();
       this.displayPatientForm = false;
@@ -77,18 +94,31 @@ export class PatientsComponent implements OnInit {
   searchPatientIdentifier(): void {
     const id = this.patientForm.value.patientIdentifier;
     this.ds.getPatientByPatientIdentifier(id).subscribe(p => {
-      console.log("The patient is:", p);
-      this.patient = {
-        patientIdentifier: p.patientIdentifier,
-        firstName: p.firstName,
-        lastName: p.lastName,
-        phone: p.phone,
-        isMinor: p.isMinor,
-        preferredLanguage: p.preferredLanguage,
-        preferredContactMethod: p.preferredContactMethod,
-      };
-      this.displayPatientFoundModal = true;
+      console.log("Get patient request returned:", p);
+      if(p.patientIdentifier) {
+        this.existingPatient = {
+          patientIdentifier: p.patientIdentifier,
+          firstName: p.firstName,
+          lastName: p.lastName,
+          phone: p.phone,
+          isMinor: p.isMinor,
+          preferredLanguage: p.preferredLanguage,
+          preferredContactMethod: p.preferredContactMethod,
+        };
+        this.displayPatientFoundModal = true;
+      } else {
+        this.displayPatientForm = true;
+      }
     })
   }
+
+  saveNewPatient(): void {
+    this.ds.addPatient().subscribe(p => {
+      console.log("Save patient response is", p);
+      alert('Success! Your patient has been saved.');
+    });
+  }
+
+  // updatePatient(): void {}
 
 }
