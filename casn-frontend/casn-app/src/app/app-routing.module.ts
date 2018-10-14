9c6876d5fb7c649ee6dd594fcf60a9231da39b65
Aspen Hollyer <aspen.dollins@gmail.com>
2018-10-14 12:55:12 -0500

    Add appts component to router

--- casn-frontend/casn-app/src/app/app-routing.module.ts
+++ casn-frontend/casn-app/src/app/app-routing.module.ts
@@ -3,14 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './auth-services/auth-guard.service';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { RidesComponent } from './rides/rides.component';
-import { PatientsComponent } from './patients/patients.component'
+import { PatientsComponent } from './patients/patients.component';
 import { LoginComponent } from './login/login.component';
+import { AppointmentsComponent } from './appointments/appointments.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full', canActivate: [ AuthGuard ] },
   { path: 'login', component: LoginComponent },
   { path: 'dashboard', component: DashboardComponent, canActivate: [ AuthGuard ] },
   { path: 'patient', component: PatientsComponent, canActivate: [ AuthGuard ] },
+  { path: 'appointment', component: AppointmentsComponent, canActivate: [ AuthGuard ] },
   // { path: '', redirectTo: '/dashboard', pathMatch: 'full', canActivate: [ AuthGuard ] },
   // { path: 'dashboard', component: DashboardComponent, canActivate: [ AuthGuard ] },
   // { path: 'facts', component: FactsComponent, canActivate: [ AuthGuard ] },
