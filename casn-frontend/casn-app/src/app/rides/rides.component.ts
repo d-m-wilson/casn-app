dbf1035dce98e3a8050b8720bc91fcbb61423e5a
Aspen Hollyer <aspen.dollins@gmail.com>
2018-11-01 13:36:40 -0500

    Set up component, fetch rides

--- casn-frontend/casn-app/src/app/rides/rides.component.ts
+++ casn-frontend/casn-app/src/app/rides/rides.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DispatcherService } from '../api/api/dispatcher.service';
 
 @Component({
   selector: 'app-rides',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./rides.component.css']
 })
 export class RidesComponent implements OnInit {
+  rides: any[];
 
-  constructor() { }
+  /*********************************************************************
+                      Constructor, Lifecycle Hooks
+  **********************************************************************/
+  // TODO: How to handle dispatcher vs. driver? Same endpoint?
+  constructor( private ds: DispatcherService ) { }
 
   ngOnInit() {
+    this.getRides()
+  }
+
+  /*********************************************************************
+                            Service Calls
+  **********************************************************************/
+  getRides(): void {
+    this.ds.getAllAppointmentsForDispatcher().subscribe(data => {
+      console.log("Appts are", data);
+      this.rides = data;
+    })
   }
 
 }
