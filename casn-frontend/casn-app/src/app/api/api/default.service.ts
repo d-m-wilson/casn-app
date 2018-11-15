a1b30e7ff9d85019c1546ae4416a077d44088508
Aspen Hollyer <aspen.dollins@gmail.com>
2018-11-15 17:41:41 -0600

    Move getAppt service calls to default service provider

--- casn-frontend/casn-app/src/app/api/api/default.service.ts
+++ casn-frontend/casn-app/src/app/api/api/default.service.ts
@@ -102,4 +102,103 @@ export class DefaultService implements DefaultServiceInterface {
         );
     }
 
+
+    /**
+     * gets appointments with driver/dispatcher-level details
+     * Get all appointments within a default date range (possibly adjustable w/ query params). Appointments include details, e.g. exact location, available only to dispatchers.
+     * @param startDate pass a startDate by which to filter
+     * @param endDate pass an endDate by which to filter
+     * @param observe set whether or not to return the data Observable as the body, response or events. defaults to returning the body.
+     * @param reportProgress flag to report request and response progress.
+     */
+    public getAllAppointments(startDate?: string, endDate?: string, observe?: 'body', reportProgress?: boolean): Observable<AllAppointments>;
+    public getAllAppointments(startDate?: string, endDate?: string, observe?: 'response', reportProgress?: boolean): Observable<HttpResponse<AllAppointments>>;
+    public getAllAppointments(startDate?: string, endDate?: string, observe?: 'events', reportProgress?: boolean): Observable<HttpEvent<AllAppointments>>;
+    public getAllAppointments(startDate?: string, endDate?: string, observe: any = 'body', reportProgress: boolean = false ): Observable<any> {
+
+        let queryParameters = new HttpParams({encoder: new CustomHttpUrlEncodingCodec()});
+        if (startDate !== undefined && startDate !== null) {
+            queryParameters = queryParameters.set('startDate', <any>startDate);
+        }
+        if (endDate !== undefined && endDate !== null) {
+            queryParameters = queryParameters.set('endDate', <any>endDate);
+        }
+
+        let headers = this.defaultHeaders;
+
+        // authentication (BearerAuth) required
+        if (this.configuration.username || this.configuration.password) {
+            headers = headers.set('Authorization', 'Basic ' + btoa(this.configuration.username + ':' + this.configuration.password));
+        }
+
+        // to determine the Accept header
+        let httpHeaderAccepts: string[] = [
+            'application/json'
+        ];
+        const httpHeaderAcceptSelected: string | undefined = this.configuration.selectHeaderAccept(httpHeaderAccepts);
+        if (httpHeaderAcceptSelected !== undefined) {
+            headers = headers.set('Accept', httpHeaderAcceptSelected);
+        }
+
+        // to determine the Content-Type header
+        const consumes: string[] = [
+        ];
+
+        return this.httpClient.get<AllAppointments>(`${this.configuration.basePath}/appointments`,
+            {
+                params: queryParameters,
+                withCredentials: this.configuration.withCredentials,
+                headers: headers,
+                observe: observe,
+                reportProgress: reportProgress
+            }
+        );
+    }
+
+
+        /**
+         * gets appointment by appointmentID
+         * Search for existing appointment by appointmentIdentifier, return dispatcher-level details
+         * @param appointmentID pass an appointmentIdentifier
+         * @param observe set whether or not to return the data Observable as the body, response or events. defaults to returning the body.
+         * @param reportProgress flag to report request and response progress.
+         */
+        public getAppointmentForDispatcherByID(appointmentID: string, observe?: 'body', reportProgress?: boolean): Observable<AppointmentDTO>;
+        public getAppointmentForDispatcherByID(appointmentID: string, observe?: 'response', reportProgress?: boolean): Observable<HttpResponse<AppointmentDTO>>;
+        public getAppointmentForDispatcherByID(appointmentID: string, observe?: 'events', reportProgress?: boolean): Observable<HttpEvent<AppointmentDTO>>;
+        public getAppointmentByID(appointmentID: string, observe: any = 'body', reportProgress: boolean = false ): Observable<any> {
+            if (appointmentID === null || appointmentID === undefined) {
+                throw new Error('Required parameter appointmentID was null or undefined when calling getAppointmentForDispatcherByID.');
+            }
+
+            let headers = this.defaultHeaders;
+
+            // authentication (BearerAuth) required
+            if (this.configuration.username || this.configuration.password) {
+                headers = headers.set('Authorization', 'Basic ' + btoa(this.configuration.username + ':' + this.configuration.password));
+            }
+
+            // to determine the Accept header
+            let httpHeaderAccepts: string[] = [
+                'application/json'
+            ];
+            const httpHeaderAcceptSelected: string | undefined = this.configuration.selectHeaderAccept(httpHeaderAccepts);
+            if (httpHeaderAcceptSelected !== undefined) {
+                headers = headers.set('Accept', httpHeaderAcceptSelected);
+            }
+
+            // to determine the Content-Type header
+            const consumes: string[] = [
+            ];
+
+            return this.httpClient.get<AppointmentDTO>(`${this.configuration.basePath}/appointments/${encodeURIComponent(String(appointmentID))}`,
+                {
+                    withCredentials: this.configuration.withCredentials,
+                    headers: headers,
+                    observe: observe,
+                    reportProgress: reportProgress
+                }
+            );
+        }
+
 }
