7b63e1fcf20845405ff8bcf7f1deed0b38cf7813
Aspen Hollyer <aspen.dollins@gmail.com>
2018-12-20 13:04:23 -0600

    Add ride display flags, clinic lookup table

--- casn-frontend/casn-app/src/app/rides/rides.component.ts
+++ casn-frontend/casn-app/src/app/rides/rides.component.ts
@@ -8,6 +8,9 @@ import { DefaultService } from '../api/api/default.service';
 })
 export class RidesComponent implements OnInit {
   rides: any[];
+  clinics: any;
+  // Display flags for rides. 0=open, 1=pending, 2=approved
+  displayRides: boolean[] = [true, true, true];
 
   /*********************************************************************
                       Constructor, Lifecycle Hooks
@@ -16,6 +19,7 @@ export class RidesComponent implements OnInit {
 
   ngOnInit() {
     this.getRides();
+    this.getClinics();
   }
 
   /*********************************************************************
@@ -23,12 +27,18 @@ export class RidesComponent implements OnInit {
   **********************************************************************/
   getRides(): void {
     // TODO: Make start/end dates dynamic
-    this.ds.getAllAppointments("2017-11-01", "2018-11-28").subscribe(data => {
-      console.log("Appts are", data);
+    this.ds.getAllAppointments("2017-11-01", "2018-11-28").subscribe(appts => {
+      console.log("Appts are:", appts);
       // Sort by date asc
-      this.rides = data.sort((a, b) => new Date(a.appointment.appointmentDate).valueOf() - new Date(b.appointment.appointmentDate).valueOf());
+      this.rides = appts.sort((a, b) => new Date(a.appointment.appointmentDate).valueOf() - new Date(b.appointment.appointmentDate).valueOf());
     })
-    console.log("Rides are:", this.rides);
+  }
+
+  getClinics(): void {
+    this.ds.getClinics().subscribe(c => {
+      console.table(c);
+      this.clinics = c.reduce((map, obj) => (map[obj.id] = obj, map), {});
+    });
   }
 
 }
