9d3b7943a852db6d893a67e13d44f822c90dc62b
Aspen Hollyer <aspen.dollins@gmail.com>
2018-12-27 16:27:03 -0600

    Display current week of skeds (cannot change yet)

--- casn-frontend/casn-app/src/app/rides/rides.component.ts
+++ casn-frontend/casn-app/src/app/rides/rides.component.ts
@@ -7,6 +7,8 @@ import { DefaultService } from '../api/api/default.service';
   styleUrls: ['./rides.component.css']
 })
 export class RidesComponent implements OnInit {
+  startDate: string = "2017-11-01";
+  endDate: string = "2018-11-28";
   rides: any[];
   clinics: any;
   // TODO: should be fetched from API or set as app-level constant
@@ -20,8 +22,9 @@ export class RidesComponent implements OnInit {
   constructor( private ds: DefaultService ) { }
 
   ngOnInit() {
-    this.getRides();
+    this.setDateRange();
     this.getClinics();
+    this.getRides();
   }
 
   /*********************************************************************
@@ -29,7 +32,7 @@ export class RidesComponent implements OnInit {
   **********************************************************************/
   getRides(): void {
     // TODO: Make start/end dates dynamic
-    this.ds.getAllAppointments("2017-11-01", "2018-11-28").subscribe(appts => {
+    this.ds.getAllAppointments(this.startDate, this.endDate).subscribe(appts => {
       console.log("Appts are:", appts);
       // Sort by date asc
       this.rides = appts.sort((a, b) => new Date(a.appointment.appointmentDate).valueOf() - new Date(b.appointment.appointmentDate).valueOf());
@@ -46,6 +49,19 @@ export class RidesComponent implements OnInit {
 /*********************************************************************
                               Utilities
 **********************************************************************/
+  setDateRange(): void {
+    const currentDate = new Date();
+    // First day of current week is day of month - day of week
+    const firstDay = currentDate.getDate() - currentDate.getDay();
+    // Last day of current week is first day + 6
+    const lastDay = firstDay + 6;
+    this.startDate = new Date(currentDate.setDate(firstDay)).toUTCString();
+    this.endDate = new Date(currentDate.setDate(lastDay)).toUTCString();
+    console.log("Current Date:", currentDate);
+    console.log("First Date:", this.startDate);
+    console.log("Last Date:", this.endDate);
+  }
+
   getStatusIcon(status: number): string {
     switch(status) {
       case 0: return "panorama_fish_eye";
