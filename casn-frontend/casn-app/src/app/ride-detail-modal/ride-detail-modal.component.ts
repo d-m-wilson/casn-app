2241cefef63834d59b87f2a3b21959fe3953891c
Aspen Hollyer <aspen.hollyer@gmail.com>
2019-01-17 13:20:19 -0600

    Display appt type in ride detail modal

--- casn-frontend/casn-app/src/app/ride-detail-modal/ride-detail-modal.component.ts
+++ casn-frontend/casn-app/src/app/ride-detail-modal/ride-detail-modal.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Constants } from '../app.constants';
 
 @Component({
   selector: 'app-ride-detail-modal',
@@ -9,9 +10,13 @@ export class RideDetailModalComponent implements OnInit {
   @Input() ride: any = {};
   @Output() closeModalClick = new EventEmitter<boolean>();
 
-  constructor() { }
+  apptTypes: any;
 
-  ngOnInit() { }
+  constructor( public constants: Constants ) { }
+
+  ngOnInit() {
+    this.apptTypes = this.constants.APPT_TYPES;
+  }
 
   handleCloseModalClick() {
     this.closeModalClick.emit(true);
