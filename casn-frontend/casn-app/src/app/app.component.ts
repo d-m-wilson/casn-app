ec4fc2a1a834f69390968c0fd37fcf15618755ed
Aspen Hollyer <aspen.hollyer@gmail.com>
2019-01-22 17:02:21 -0600

    Make sidenav slightly less awful

--- casn-frontend/casn-app/src/app/app.component.ts
+++ casn-frontend/casn-app/src/app/app.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Constants } from './app.constants';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   opened: boolean;
-  title = 'casn-app';
+  menuItems: any[];
+
+  constructor( private constants: Constants ) { }
+
+  ngOnInit() {
+    this.menuItems = this.constants.MENU_ITEMS;
+  }
 }
