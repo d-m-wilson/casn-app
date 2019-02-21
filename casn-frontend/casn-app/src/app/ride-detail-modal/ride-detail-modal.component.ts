809c17ced15673e95f266b365e7d06733a85e14f
Aspen Hollyer <aspen.hollyer@gmail.com>
2019-02-21 15:45:42 -0600

    Display caller notes on ride detail modal (PH for now)

--- casn-frontend/casn-app/src/app/ride-detail-modal/ride-detail-modal.component.ts
+++ casn-frontend/casn-app/src/app/ride-detail-modal/ride-detail-modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { DefaultService } from '../api/api/default.service';
 import { DriverService } from '../api/api/driver.service';
 import { DispatcherService } from '../api/api/dispatcher.service';
