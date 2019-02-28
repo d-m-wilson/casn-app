a6f6ab2bc00c121283dd93cec5b7ec8c72c0a165
Aspen Hollyer <aspen.hollyer@gmail.com>
2019-02-28 17:37:40 -0600

    Create map component for alternate rides view

--- casn-frontend/casn-app/src/app/map/map.component.ts
+++ casn-frontend/casn-app/src/app/map/map.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-map',
@@ -6,10 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./map.component.css']
 })
 export class MapComponent implements OnInit {
+  @Input() rides: any = {};
+  @Output() closeModalClick = new EventEmitter<boolean>();
+  mapCenter: any = { latitude: 29.7604, longitude: -95.3698, zoom: 11 };
+  coords: any[];
 
   constructor() { }
 
   ngOnInit() {
+    this.coords = [
+      [29.767381, -95.349843],
+      [29.783622, -95.322733],
+      [29.787346, -95.355677],
+      [29.759036, -95.378155],
+      [29.737873, -95.416263],
+      [29.703232, -95.408203]
+    ]
+  }
+
+/*********************************************************************
+                            Click Handlers
+**********************************************************************/
+  handleCloseModalClick() {
+    this.closeModalClick.emit(true);
   }
 
 }
