f193ef6e9026d12d2f112a88e0c6593a9c02346e
Aspen Hollyer <aspen.hollyer@gmail.com>
2019-02-28 14:03:29 -0600

    Add map with start/end markers (dummy lat/lng for now)

--- casn-frontend/casn-app/src/app/ride-detail-modal/ride-detail-modal.component.ts
+++ casn-frontend/casn-app/src/app/ride-detail-modal/ride-detail-modal.component.ts
@@ -18,6 +18,10 @@ export class RideDetailModalComponent implements OnInit {
   apptTypes: any;
   clinics: any;
   volunteers: any[];
+  lat: number = 51.678418;
+  lng: number = 7.809007;
+  lat2: number = 51.970418;
+  lng2: number = 7.400007;
 
   constructor( private ds: DefaultService,
                private driverService: DriverService,
@@ -30,6 +34,7 @@ export class RideDetailModalComponent implements OnInit {
     this.getClinics();
     this.getVolunteers();
     this.driveType = this.isDriveTo ? 'driveTo' : 'driveFrom';
+    console.log(this.ride);
   }
 
 /*********************************************************************
