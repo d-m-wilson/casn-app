28ec60ba8934554f54bdace6aa1a9332df5d8ba9
Aspen Hollyer <aspen.hollyer@gmail.com>
2019-03-07 17:38:38 -0600

    Add address links to map

--- casn-frontend/casn-app/src/app/map/map.component.ts
+++ casn-frontend/casn-app/src/app/map/map.component.ts
@@ -21,7 +21,23 @@ export class MapComponent implements OnInit {
       [29.759036, -95.378155],
       [29.737873, -95.416263],
       [29.703232, -95.408203]
-    ]
+    ];
+    this.addFakeCoordsToRides();
+  }
+
+  addFakeCoordsToRides() {
+    this.rides.forEach(r => {
+      r.driveTo.startLatitude = this.coords[0][0];
+      r.driveTo.startLongitude = this.coords[0][1];
+      r.driveTo.endLatitude = this.coords[1][0];
+      r.driveTo.endLongitude = this.coords[1][1];
+
+      r.driveFrom.startLatitude = this.coords[2][0];
+      r.driveFrom.startLongitude = this.coords[2][1];
+      r.driveFrom.endLatitude = this.coords[3][0];
+      r.driveFrom.endLongitude = this.coords[3][1];
+    });
+    console.log("Rides with fake coords", this.rides);
   }
 
 /*********************************************************************
@@ -31,4 +47,12 @@ export class MapComponent implements OnInit {
     this.closeMapModalClick.emit(true);
   }
 
+/*********************************************************************
+                    Utilities - Getters/Setters, etc.
+**********************************************************************/
+  getGoogleMapLink(ride: any, driveType: string, addressType: string) {
+    const query = `${ride[driveType][addressType + 'Address']} ${ride[driveType][addressType + 'City']} ${ride[driveType][addressType + 'PostalCode']}`;
+    const urlEncodedQuery = encodeURI(query);
+    return `https://www.google.com/maps/search/?api=1&query=${urlEncodedQuery}`;
+  }
 }
