50285a403b5a934f500cca88043a46db902976be
Aspen Hollyer <aspen.hollyer@gmail.com>
2019-04-18 09:06:58 -0500

    Update appt types in app constants file

--- casn-frontend/casn-app/src/app/app.constants.ts
+++ casn-frontend/casn-app/src/app/app.constants.ts
@@ -1,6 +1,11 @@
 /* Define app-level constants which can be imported to any module */
 export class Constants {
-    public readonly APPT_TYPES = { 4: 'Ultrasound', 3: 'Surgical' };
+    public readonly APPT_TYPES = { 1: 'Lam to Complete',
+                                   2: 'Lam Insert',
+                                   3: 'Surgical',
+                                   4: 'Ultrasound',
+                                   5: 'Courthouse Appointment',
+                                   6: 'Follow up Appointment' };
 
     public readonly MENU_ITEMS = [
       {name: "My Dashboard", address: "/", icon: "dashboard"},
