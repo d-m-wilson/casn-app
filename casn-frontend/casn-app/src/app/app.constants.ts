0e8ec4fbc8d6316265721e82ba54255dd30c2193
Aspen Hollyer <aspen.hollyer@gmail.com>
2019-04-19 12:16:50 -0500

    Delete APPT_TYPES from app constants

--- casn-frontend/casn-app/src/app/app.constants.ts
+++ casn-frontend/casn-app/src/app/app.constants.ts
@@ -1,12 +1,5 @@
 /* Define app-level constants which can be imported to any module */
 export class Constants {
-    public readonly APPT_TYPES = { 1: 'Lam to Complete',
-                                   2: 'Lam Insert',
-                                   3: 'Surgical',
-                                   4: 'Ultrasound',
-                                   5: 'Courthouse Appointment',
-                                   6: 'Follow up Appointment' };
-
     public readonly MENU_ITEMS = [
       {name: "My Dashboard", address: "/", icon: "dashboard"},
       {name: "Schedule a Ride", address: "/caller", icon: "departure_board"},
