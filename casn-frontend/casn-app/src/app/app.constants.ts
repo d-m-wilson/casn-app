41996df5258b5d99260c0a1c617a6b3dd6353921
Aspen Hollyer <aspen.hollyer@gmail.com>
2019-05-18 18:05:14 -0500

    Add user stats UI to app menu

--- casn-frontend/casn-app/src/app/app.constants.ts
+++ casn-frontend/casn-app/src/app/app.constants.ts
@@ -1,7 +1,8 @@
 /* Define app-level constants which can be imported to any module */
 export class Constants {
     public readonly MENU_ITEMS = [
-      {name: "My Dashboard", address: "/", icon: "dashboard"},
+      {name: "Home", address: "/", icon: "dashboard"},
+      {name: "My Profile", address: "/stats", icon: "account_circle"},
       {name: "Schedule a Ride", address: "/caller", icon: "departure_board"},
       {name: "View Schedule", address: "/view-schedule", icon: "event_note"},
     ];
