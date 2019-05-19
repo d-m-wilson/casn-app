a0e091ef3ef3568f5a6dc36471ae43747adaf136
Aspen Hollyer <aspen.hollyer@gmail.com>
2019-05-18 20:58:24 -0500

    Create very simple user stats screen

--- casn-frontend/casn-app/src/app/user-stats/user-stats.component.ts
+++ casn-frontend/casn-app/src/app/user-stats/user-stats.component.ts
@@ -3,13 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-user-stats',
   templateUrl: './user-stats.component.html',
-  styleUrls: ['./user-stats.component.css']
+  styleUrls: ['./user-stats.component.scss']
 })
 export class UserStatsComponent implements OnInit {
+  badges: any[] = new Array(15);
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
 }
