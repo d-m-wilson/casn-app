4420d977d1a8f548fdcd1bd0dcaa5824e40a5676
Aspen Hollyer <aspen.hollyer@gmail.com>
2019-05-30 15:59:36 -0500

    Mock up service for returning badges for user

---
+++ casn-frontend/casn-app/src/app/user-stats/temp-badges.ts
@@ -0,0 +1,130 @@
+export const tempBadges = [
+  {
+    "id": 1,
+    "title": "First Drive",
+    "description": "You earned the achievement for completing your first drive",
+    "earned": true,
+    "path": "/assets/badges/1.png",
+    "dateEarned": "1/2/2019"
+  },
+  {
+    "id": 2,
+    "title": "First Drive",
+    "description": "You earned the achievement for completing your first drive",
+    "earned": false,
+    "path": "/assets/badges/2.png",
+    "dateEarned": "1/2/2019"
+  },
+  {
+    "id": 3,
+    "title": "First Drive",
+    "description": "You earned the achievement for completing your first drive",
+    "earned": false,
+    "path": "/assets/badges/3.png",
+    "dateEarned": "1/2/2019"
+  },
+  {
+    "id": 4,
+    "title": "First Drive",
+    "description": "You earned the achievement for completing your first drive",
+    "earned": false,
+    "path": "/assets/badges/4.png",
+    "dateEarned": "1/2/2019"
+  },
+  {
+    "id": 5,
+    "title": "First Drive",
+    "description": "You earned the achievement for completing your first drive",
+    "earned": true,
+    "path": "/assets/badges/5.png",
+    "dateEarned": "1/2/2019"
+  },
+  {
+    "id": 6,
+    "title": "First Drive",
+    "description": "You earned the achievement for completing your first drive",
+    "earned": true,
+    "path": "/assets/badges/6.png",
+    "dateEarned": "1/2/2019"
+  },
+  {
+    "id": 7,
+    "title": "First Drive",
+    "description": "You earned the achievement for completing your first drive",
+    "earned": true,
+    "path": "/assets/badges/7.png",
+    "dateEarned": "1/2/2019"
+  },
+  {
+    "id": 8,
+    "title": "First Drive",
+    "description": "You earned the achievement for completing your first drive",
+    "earned": false,
+    "path": "/assets/badges/8.png",
+    "dateEarned": "1/2/2019"
+  },
+  {
+    "id": 9,
+    "title": "First Drive",
+    "description": "You earned the achievement for completing your first drive",
+    "earned": true,
+    "path": "/assets/badges/9.png",
+    "dateEarned": "1/2/2019"
+  },
+  {
+    "id": 10,
+    "title": "First Drive",
+    "description": "You earned the achievement for completing your first drive",
+    "earned": false,
+    "path": "/assets/badges/10.png",
+    "dateEarned": "1/2/2019"
+  },
+  {
+    "id": 11,
+    "title": "First Drive",
+    "description": "You earned the achievement for completing your first drive",
+    "earned": false,
+    "path": "/assets/badges/11.png",
+    "dateEarned": "1/2/2019"
+  },
+  {
+    "id": 12,
+    "title": "First Drive",
+    "description": "You earned the achievement for completing your first drive",
+    "earned": false,
+    "path": "/assets/badges/12.png",
+    "dateEarned": "1/2/2019"
+  },
+  {
+    "id": 13,
+    "title": "First Drive",
+    "description": "You earned the achievement for completing your first drive",
+    "earned": false,
+    "path": "/assets/badges/13.png",
+    "dateEarned": "1/2/2019"
+  },
+  {
+    "id": 14,
+    "title": "First Drive",
+    "description": "You earned the achievement for completing your first drive",
+    "earned": true,
+    "path": "/assets/badges/14.png",
+    "dateEarned": "1/2/2019"
+  },
+  {
+    "id": 15,
+    "title": "First Drive",
+    "description": "You earned the achievement for completing your first drive",
+    "earned": true,
+    "path": "/assets/badges/15.png",
+    "dateEarned": "1/2/2019"
+  },
+  {
+    "id": 16,
+    "title": "First Drive",
+    "description": "You earned the achievement for completing your first drive",
+    "earned": false,
+    "path": "/assets/badges/16.png",
+    "dateEarned": "1/2/2019"
+  },
+]
