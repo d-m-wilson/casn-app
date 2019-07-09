96fcc78eff7c753e08663c9ed55cc2ae4d107256
Aspen Hollyer <aspen.hollyer@gmail.com>
2019-07-09 16:23:11 -0500

    Redirect drivers to ride list page on login

--- casn-frontend/casn-app/src/app/dashboard/dashboard.component.ts
+++ casn-frontend/casn-app/src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
@@ -8,10 +9,15 @@ import { Component, OnInit } from '@angular/core';
 export class DashboardComponent implements OnInit {
   userRole: string;
 
-  constructor() { }
+  constructor( private router: Router ) { }
 
   ngOnInit() {
     this.userRole = localStorage.getItem("userRole");
+    if(this.userRole === '2') this.redirectDriverToSchedule();
+  }
+
+  redirectDriverToSchedule(): void {
+    this.router.navigate(['/view-schedule']);
   }
 
 }
