f68c8c7453c74cc851bd865a96cf4f7ed7e0eca5
Aspen Hollyer <aspen.hollyer@gmail.com>
2019-07-10 14:47:04 -0500

    Add basic message textarea & user table

--- casn-frontend/casn-app/src/app/mass-text/mass-text.component.ts
+++ casn-frontend/casn-app/src/app/mass-text/mass-text.component.ts
@@ -1,15 +1,61 @@
 import { Component, OnInit } from '@angular/core';
+import { SelectionModel } from '@angular/cdk/collections';
+import { MatTableDataSource } from '@angular/material/table';
+
+export interface PeriodicElement {
+  name: string;
+  position: number;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Hydrogen'},
+  {position: 2, name: 'Helium'},
+  {position: 3, name: 'Lithium'},
+  {position: 4, name: 'Beryllium'},
+  {position: 5, name: 'Boron'},
+  {position: 6, name: 'Carbon'},
+  {position: 7, name: 'Nitrogen'},
+  {position: 8, name: 'Oxygen'},
+  {position: 9, name: 'Fluorine'},
+  {position: 10, name: 'Neon'},
+];
 
 @Component({
   selector: 'app-mass-text',
   templateUrl: './mass-text.component.html',
-  styleUrls: ['./mass-text.component.css']
+  styleUrls: ['./mass-text.component.scss']
 })
 export class MassTextComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  displayedColumns: string[] = ['select', 'name'];
+  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
+  selection = new SelectionModel<PeriodicElement>(true, []);
+
+  /** Whether the number of selected elements matches the total number of rows. */
+  isAllSelected() {
+    const numSelected = this.selection.selected.length;
+    const numRows = this.dataSource.data.length;
+    return numSelected === numRows;
+  }
+
+  /** Selects all rows if they are not all selected; otherwise clear selection. */
+  masterToggle() {
+    this.isAllSelected() ?
+        this.selection.clear() :
+        this.dataSource.data.forEach(row => this.selection.select(row));
+  }
+
+  /** The label for the checkbox on the passed row */
+  checkboxLabel(row?: PeriodicElement): string {
+    if (!row) {
+      return `${this.isAllSelected() ? 'select' : 'deselect'} all`;
+    }
+    return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${row.position + 1}`;
   }
 
 }
