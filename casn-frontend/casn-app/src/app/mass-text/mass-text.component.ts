0f9cb0a12e37c32555f60b50d80d147104aa57e8
Aspen Hollyer <aspen.hollyer@gmail.com>
2019-07-11 16:18:50 -0500

    Add placeholder method to send message

--- casn-frontend/casn-app/src/app/mass-text/mass-text.component.ts
+++ casn-frontend/casn-app/src/app/mass-text/mass-text.component.ts
@@ -13,11 +13,11 @@ const PERSON_DATA: UserContact[] = [
   {position: 3, name: 'David'},
   {position: 4, name: 'Katie'},
   {position: 5, name: 'Naima'},
-  {position: 6, name: 'Aspen'},
-  {position: 7, name: 'Brian'},
-  {position: 8, name: 'David'},
-  {position: 9, name: 'Katie'},
-  {position: 10, name: 'Naima'},
+  {position: 6, name: 'Aaaaa'},
+  {position: 7, name: 'Bbbbb'},
+  {position: 8, name: 'Ccccc'},
+  {position: 9, name: 'Ddddd'},
+  {position: 10, name: 'Eeeee'},
 ];
 
 @Component({
@@ -26,6 +26,7 @@ const PERSON_DATA: UserContact[] = [
   styleUrls: ['./mass-text.component.scss']
 })
 export class MassTextComponent implements OnInit {
+  message: string = '';
 
   constructor() { }
 
@@ -36,26 +37,34 @@ export class MassTextComponent implements OnInit {
   dataSource = new MatTableDataSource<UserContact>(PERSON_DATA);
   selection = new SelectionModel<UserContact>(true, []);
 
-  /** Whether the number of selected elements matches the total number of rows. */
   isAllSelected() {
     const numSelected = this.selection.selected.length;
     const numRows = this.dataSource.data.length;
     return numSelected === numRows;
   }
 
-  /** Selects all rows if they are not all selected; otherwise clear selection. */
   masterToggle() {
     this.isAllSelected() ?
         this.selection.clear() :
         this.dataSource.data.forEach(row => this.selection.select(row));
   }
 
-  /** The label for the checkbox on the passed row */
   checkboxLabel(row?: UserContact): string {
     if (!row) {
       return `${this.isAllSelected() ? 'select' : 'deselect'} all`;
     }
     return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${row.position + 1}`;
+  }
+
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  sendMessage() {
+    const recipient = this.selection.selected.length + ' recipients';
+    if(confirm(`Send message to ${recipient}?`)) {
+      console.log("--Logic to send msg goes here")
+    }
   }
 
 }
