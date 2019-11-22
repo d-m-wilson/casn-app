7498158be9f00988825b1ee61e909589330c4852
Aspen Hollyer <aspen.hollyer@gmail.com>
2019-11-22 12:11:28 -0600

    Do not validate callerIdentifier unless user tries to change it

--- casn-frontend/casn-app/src/app/callers/caller-identifier.validator.ts
+++ casn-frontend/casn-app/src/app/callers/caller-identifier.validator.ts
@@ -11,9 +11,14 @@ export class CallerIdentifierValidator implements AsyncValidator {
                 private route: ActivatedRoute ) {}
 
   validate(callerIdentifier: FormControl): Promise<ValidationErrors | null> | Observable<ValidationErrors | null> {
+    // This validator only matters if the user is editing an existing caller.
     const editing = !!this.route.snapshot.paramMap.get('callerIdentifier');
-    console.log("User is editing?", editing);
     if(!editing) return of(null);
+
+    // If the user hasn't changed the callerIdentifier, we don't need
+    // to perform a network request to check if it's taken.
+    const callerIdentifierToEdit = this.route.snapshot.paramMap.get('callerIdentifier');
+    if(callerIdentifierToEdit == callerIdentifier.value ) return of(null);
 
     // If user is editing a caller, throw a form validation error if they
     // change the callerIdentifier to one that is already in use.
