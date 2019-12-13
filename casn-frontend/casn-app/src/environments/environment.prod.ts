de48b55f806a599efff4949b9ffd1f31d35b3f57
David M. Wilson <david.maurice.wilson@gmail.com>
2019-12-13 14:51:41 -0600

    Attempting to reconfigure Angular app for new backend location

--- casn-frontend/casn-app/src/environments/environment.prod.ts
+++ casn-frontend/casn-app/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://test.casn.app/api',
+  apiUrl: 'https://testapi.casn.app/api',
   clientRoot: 'https://test.casn.app/app/',
   customConfigUrl: 'https://test.casn.app/customconfig.json',
   get logoutRedirectUrl() { return `${this.clientRoot}assets/oidc-login-redirect.html`; },
