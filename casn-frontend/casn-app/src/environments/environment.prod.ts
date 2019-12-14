1c87b0bc4bbbf6fdf3a013baab5e7f68a36bfc94
David M. Wilson <david.maurice.wilson@gmail.com>
2019-12-13 22:29:17 -0600

    Updated environment.prod.ts: removed /app prefix

--- casn-frontend/casn-app/src/environments/environment.prod.ts
+++ casn-frontend/casn-app/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   apiUrl: 'https://testapi.casn.app/api',
-  clientRoot: 'https://test.casn.app/app/',
+  clientRoot: 'https://test.casn.app/',
   customConfigUrl: 'https://test.casn.app/customconfig.json',
   get logoutRedirectUrl() { return `${this.clientRoot}assets/oidc-login-redirect.html`; },
 };
