17e210719054c2390c4888d886ecab4cb8d1d22e
David M. Wilson <david.maurice.wilson@gmail.com>
2019-12-23 20:50:46 -0600

    Updated ng config files for prod

--- casn-frontend/casn-app/src/environments/environment.prod.ts
+++ casn-frontend/casn-app/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://testapi.casn.app/api',
-  clientRoot: 'https://test.casn.app/',
-  customConfigUrl: 'https://test.casn.app/customconfig.json',
+  apiUrl: 'https://api.casn.app/api',
+  clientRoot: 'https://casn.app/',
+  customConfigUrl: 'https://casn.app/customconfig.json',
   get logoutRedirectUrl() { return `${this.clientRoot}assets/oidc-login-redirect.html`; },
 };
