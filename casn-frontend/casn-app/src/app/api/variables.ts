154b93951e2fa98dab033f3a618b0cdbe20c9b9d
Aspen Hollyer <aspen.hollyer@gmail.com>
2019-12-23 18:17:04 -0600

    Pull API URL from environment

--- casn-frontend/casn-app/src/app/api/variables.ts
+++ casn-frontend/casn-app/src/app/api/variables.ts
@@ -1,6 +1,7 @@
 import { InjectionToken } from '@angular/core';
+import { environment } from '../../environments/environment';
 
-export const BASE_PATH = 'https://testapi.casn.app/api'
+export const BASE_PATH = environment.apiUrl;
 export const COLLECTION_FORMATS = {
     'csv': ',',
     'tsv': '   ',
