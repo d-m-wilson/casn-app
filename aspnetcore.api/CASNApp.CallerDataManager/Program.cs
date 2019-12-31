8c699c986f44abc16080d6593d438b69dff9cfe0
David M. Wilson <david.maurice.wilson@gmail.com>
2019-12-30 18:20:28 -0600

    Added Caller Data Manager webjob

---
+++ aspnetcore.api/CASNApp.CallerDataManager/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace CASNApp.CallerDataManager
+{
+    class Program
+    {
+        private static readonly IConfiguration configuration;
+        private static readonly string callerDataProcedureName;
+        private static readonly int callerDataDaysToKeep;
+
+        static Program()
+        {
+            configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", false, false)
+                .AddUserSecrets<Program>(true)
+                .AddEnvironmentVariables()
+                .Build();
+
+            callerDataDaysToKeep = int.Parse(configuration["CallerDataDaysToKeep"]);
+            callerDataProcedureName = configuration["CallerDataProcedureName"];
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                using (var sqlConnection = new SqlConnection(configuration.GetConnectionString("DbConnectionString")))
+                {
+                    sqlConnection.Open();
+
+                    using (var sqlCommand = new SqlCommand(callerDataProcedureName, sqlConnection))
+                    {
+                        sqlCommand.CommandType = System.Data.CommandType.StoredProcedure;
+                        sqlCommand.Parameters.AddWithValue("daysToKeep", callerDataDaysToKeep);
+                        sqlCommand.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine($"{DateTime.Now:yyyy-MM-dd HH:mm:ss}: Fatal Exception: {ex}");
+                throw new Exception("Re-throwing exception. ", ex);
+            }
+        }
+    }
+}
---
+++ aspnetcore.api/CASNApp.CallerDataManager/run.cmd
@@ -0,0 +1,1 @@
+﻿CASNApp.CallerDataManager.exe
