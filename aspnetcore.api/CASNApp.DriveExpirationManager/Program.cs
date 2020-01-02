72231bab44ebc58678ea4470e5958a747ddbfc7f
David M. Wilson <david.maurice.wilson@gmail.com>
2020-01-02 17:01:05 -0600

    Added Drive Expiration Manager webjob

---
+++ aspnetcore.api/CASNApp.DriveExpirationManager/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace CASNApp.DriveExpirationManager
+{
+    class Program
+    {
+        private static readonly IConfiguration configuration;
+        private static readonly string driveExpirationProcedureName;
+
+        static Program()
+        {
+            configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", false, false)
+                .AddUserSecrets<Program>(true)
+                .AddEnvironmentVariables()
+                .Build();
+
+            driveExpirationProcedureName = configuration["DriveExpirationProcedureName"];
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                using (var sqlConnection = new SqlConnection(configuration.GetConnectionString("DbConnectionString")))
+                {
+                    sqlConnection.Open();
+
+                    using (var sqlCommand = new SqlCommand(driveExpirationProcedureName, sqlConnection))
+                    {
+                        sqlCommand.CommandType = System.Data.CommandType.StoredProcedure;
+                        sqlCommand.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine($"{DateTime.Now:yyyy-MM-dd HH:mm:ss}: Fatal Exception: {ex}");
+                throw new Exception("Re-throwing exception. ", ex);
+            }
+        }
+    }
+}
---
+++ aspnetcore.api/CASNApp.DriveExpirationManager/run.cmd
@@ -0,0 +1,1 @@
+CASNApp.DriveExpirationManager.exe
