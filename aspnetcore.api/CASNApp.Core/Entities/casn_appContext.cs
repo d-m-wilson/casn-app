d-m-wilson/casn-app

commit 816d4d290a1d9f0157d2006ed75a5353d5c3c29a

Updated EF DbContext class: Pluralized the names of all DbSet properties

aspnetcore.api/CASNApp.Core/Entities/casn_appContext.cs
@@ -38,23 +38,23 @@ namespace CASNApp.Core.Entities
             }
         }
 
-        public virtual DbSet<Appointment> Appointment { get; set; }
-        public virtual DbSet<AppointmentType> AppointmentType { get; set; }
-        public virtual DbSet<Badge> Badge { get; set; }
-        public virtual DbSet<Caller> Caller { get; set; }
-        public virtual DbSet<Drive> Drive { get; set; }
-        public virtual DbSet<DriveCancelReason> DriveCancelReason { get; set; }
-        public virtual DbSet<DriveLogStatus> DriveLogStatus { get; set; }
-        public virtual DbSet<DriveStatus> DriveStatus { get; set; }
-        public virtual DbSet<Message> Message { get; set; }
-        public virtual DbSet<MessageErrorLog> MessageErrorLog { get; set; }
-        public virtual DbSet<MessageLog> MessageLog { get; set; }
-        public virtual DbSet<MessageType> MessageType { get; set; }
-        public virtual DbSet<ServiceProvider> ServiceProvider { get; set; }
-        public virtual DbSet<ServiceProviderType> ServiceProviderType { get; set; }
-        public virtual DbSet<Volunteer> Volunteer { get; set; }
-        public virtual DbSet<VolunteerBadge> VolunteerBadge { get; set; }
-        public virtual DbSet<VolunteerDriveLog> VolunteerDriveLog { get; set; }
+        public virtual DbSet<Appointment> Appointments { get; set; }
+        public virtual DbSet<AppointmentType> AppointmentTypes { get; set; }
+        public virtual DbSet<Badge> Badges { get; set; }
+        public virtual DbSet<Caller> Callers { get; set; }
+        public virtual DbSet<Drive> Drives { get; set; }
+        public virtual DbSet<DriveCancelReason> DriveCancelReasons { get; set; }
+        public virtual DbSet<DriveLogStatus> DriveLogStatuses { get; set; }
+        public virtual DbSet<DriveStatus> DriveStatuses { get; set; }
+        public virtual DbSet<Message> Messages { get; set; }
+        public virtual DbSet<MessageErrorLog> MessageErrorLogs { get; set; }
+        public virtual DbSet<MessageLog> MessageLogs { get; set; }
+        public virtual DbSet<MessageType> MessageTypes { get; set; }
+        public virtual DbSet<ServiceProvider> ServiceProviders { get; set; }
+        public virtual DbSet<ServiceProviderType> ServiceProviderTypes { get; set; }
+        public virtual DbSet<Volunteer> Volunteers { get; set; }
+        public virtual DbSet<VolunteerBadge> VolunteerBadges { get; set; }
+        public virtual DbSet<VolunteerDriveLog> VolunteerDriveLogs { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {

aspnetcore.api/CASNApp.API/Controllers/DefaultApi.cs
@@ -59,7 +59,7 @@ namespace CASNApp.API.Controllers
                 return Forbid();
             }
 
-            var serviceProviders = await dbContext.ServiceProvider
+            var serviceProviders = await dbContext.ServiceProviders
                 .AsNoTracking()
                 .Include(sp => sp.ServiceProviderType)
                 .Select(c => new Core.Models.ServiceProvider(c))
@@ -103,7 +103,7 @@ namespace CASNApp.API.Controllers
                 end = end.Add(new TimeSpan(23, 59, 59));
             }
 
-            var appointmentEntities = await dbContext.Appointment
+            var appointmentEntities = await dbContext.Appointments
                 .AsNoTracking()
                 .Include(a => a.Drives)
                 .Include(a => a.Caller)
@@ -113,7 +113,7 @@ namespace CASNApp.API.Controllers
                             a.CallerId.HasValue)
                 .ToListAsync();
 
-            var drivesUserHasAppliedFor = await dbContext.VolunteerDriveLog
+            var drivesUserHasAppliedFor = await dbContext.VolunteerDriveLogs
                 .AsNoTracking()
                 .Where(vdl => vdl.VolunteerId == volunteer.Id &&
                     vdl.DriveLogStatusId == DriveLogStatus.APPLIED &&
@@ -201,7 +201,7 @@ namespace CASNApp.API.Controllers
                 return BadRequest();
             }
 
-            var apptEntity = await dbContext.Appointment
+            var apptEntity = await dbContext.Appointments
                 .AsNoTracking()
                 .Include(a => a.Drives)
                 .Include(a => a.Caller)

aspnetcore.api/CASNApp.API/Controllers/DispatcherApi.cs
@@ -91,7 +91,7 @@ namespace CASNApp.API.Controllers
             var driveTo = appointmentDTO.DriveTo;
             var driveFrom = appointmentDTO.DriveFrom;
 
-            var serviceProvider = await dbContext.ServiceProvider
+            var serviceProvider = await dbContext.ServiceProviders
                 .AsNoTracking()
                 .Where(c => c.Id == appointment.ServiceProviderId)
                 .FirstOrDefaultAsync();
@@ -101,7 +101,7 @@ namespace CASNApp.API.Controllers
                 return BadRequest(appointmentDTO);
             }
 
-            var caller = await dbContext.Caller
+            var caller = await dbContext.Callers
                 .AsNoTracking()
                 .Where(p => p.Id == appointment.CallerId)
                 .FirstOrDefaultAsync();
@@ -247,16 +247,16 @@ namespace CASNApp.API.Controllers
                 appointmentEntity.DropoffVagueLongitude = dropoffVagueLocation.Longitude;
             }
 
-            dbContext.Appointment.Add(appointmentEntity);
+            dbContext.Appointments.Add(appointmentEntity);
 
             if (driveToEntity != null)
             {
-                dbContext.Drive.Add(driveToEntity);
+                dbContext.Drives.Add(driveToEntity);
             }
 
             if (driveFromEntity != null)
             {
-                dbContext.Drive.Add(driveFromEntity);
+                dbContext.Drives.Add(driveFromEntity);
             }
 
             await dbContext.SaveChangesAsync();
@@ -464,7 +464,7 @@ namespace CASNApp.API.Controllers
 
             var driveId = body.DriveId.Value;
 
-            var drive = await dbContext.Drive
+            var drive = await dbContext.Drives
                 .Where(d => d.Id == driveId && d.IsActive)
                 .SingleOrDefaultAsync();
 
@@ -482,7 +482,7 @@ namespace CASNApp.API.Controllers
 
             var driverId = drive.DriverId.Value;
 
-            var volunteerDriveLogsForThisDrive = await dbContext.VolunteerDriveLog
+            var volunteerDriveLogsForThisDrive = await dbContext.VolunteerDriveLogs
                 .Where(vd => vd.DriveId == driveId &&
                     vd.IsActive &&
                     (vd.DriveLogStatusId == Core.Entities.DriveLogStatus.APPLIED ||
@@ -571,14 +571,14 @@ namespace CASNApp.API.Controllers
                     {
                         logger.LogInformation($"Checking badges for VolunteerDriveLog #{volunteerDriveLog.Id}");
 
-                        var volunteerBadges = await dbContext.VolunteerBadge
+                        var volunteerBadges = await dbContext.VolunteerBadges
                             .Where(vb => vb.VolunteerDriveLogId == volunteerDriveLog.Id)
                             .ToListAsync();
 
                         foreach (var volunteerBadge in volunteerBadges)
                         {
                             logger.LogInformation($"Removing VolunteerBadge #{volunteerBadge.Id} for badge #{volunteerBadge.BadgeId}");
-                            dbContext.VolunteerBadge.Remove(volunteerBadge);
+                            dbContext.VolunteerBadges.Remove(volunteerBadge);
                         }
                     }
 
@@ -638,7 +638,7 @@ namespace CASNApp.API.Controllers
                 return Conflict(body1);
             }
 
-            var volunteerDriveLogsForThisDrive = await dbContext.VolunteerDriveLog
+            var volunteerDriveLogsForThisDrive = await dbContext.VolunteerDriveLogs
                 .Where(vd => vd.DriveId == drive.Id &&
                     vd.IsActive &&
                     (vd.DriveLogStatusId == Core.Entities.DriveLogStatus.APPLIED ||
@@ -710,14 +710,14 @@ namespace CASNApp.API.Controllers
                     {
                         logger.LogInformation($"Checking badges for VolunteerDriveLog #{volunteerDriveLog.Id}");
 
-                        var volunteerBadges = await dbContext.VolunteerBadge
+                        var volunteerBadges = await dbContext.VolunteerBadges
                             .Where(vb => vb.VolunteerDriveLogId == volunteerDriveLog.Id)
                             .ToListAsync();
 
                         foreach (var volunteerBadge in volunteerBadges)
                         {
                             logger.LogInformation($"Removing VolunteerBadge #{volunteerBadge.Id} for badge #{volunteerBadge.BadgeId}");
-                            dbContext.VolunteerBadge.Remove(volunteerBadge);
+                            dbContext.VolunteerBadges.Remove(volunteerBadge);
                         }
                     }
 
@@ -828,7 +828,7 @@ namespace CASNApp.API.Controllers
                 return BadRequest();
             }
 
-            var exists = await dbContext.Caller
+            var exists = await dbContext.Callers
                 .Where(p => p.CallerIdentifier == caller.CallerIdentifier)
                 .AnyAsync();
 
@@ -839,7 +839,7 @@ namespace CASNApp.API.Controllers
 
             var callerEntity = new Core.Entities.Caller(caller);
 
-            dbContext.Caller.Add(callerEntity);
+            dbContext.Callers.Add(callerEntity);
             await dbContext.SaveChangesAsync();
 
             caller.Id = callerEntity.Id;
@@ -912,7 +912,7 @@ namespace CASNApp.API.Controllers
             //TODO: Uncomment the next line to return response 404 or use other options such as return this.NotFound(), return this.BadRequest(..), ...
             // return StatusCode(404);
 
-            var callers = await dbContext.Caller
+            var callers = await dbContext.Callers
                 .AsNoTracking()
                 .Where(p => p.CallerIdentifier == callerIdentifier)
                 .Select(p => new Core.Models.Caller(p))
@@ -954,12 +954,12 @@ namespace CASNApp.API.Controllers
                 return Forbid();
             }
 
-            if (!driveId.HasValue || !dbContext.Drive.Any(d => d.Id == driveId.Value))
+            if (!driveId.HasValue || !dbContext.Drives.Any(d => d.Id == driveId.Value))
             {
                 return BadRequest();
             }
 
-            var results = dbContext.VolunteerDriveLog
+            var results = dbContext.VolunteerDriveLogs
                 .AsNoTracking()
                 .Include(vdl => vdl.Volunteer)
                 .Where(vdl => vdl.DriveId == driveId.Value && vdl.IsActive)
@@ -1013,7 +1013,7 @@ namespace CASNApp.API.Controllers
 
             var appointmentModel = appointmentDTO.Appointment;
 
-            var serviceProvider = await dbContext.ServiceProvider
+            var serviceProvider = await dbContext.ServiceProviders
                 .AsNoTracking()
                 .Where(c => c.Id == appointmentModel.ServiceProviderId && c.IsActive)
                 .FirstOrDefaultAsync();
@@ -1023,7 +1023,7 @@ namespace CASNApp.API.Controllers
                 return BadRequest(appointmentDTO);
             }
 
-            var callerEntity = await dbContext.Caller
+            var callerEntity = await dbContext.Callers
                 .Where(c => c.Id == appointmentModel.CallerId && c.IsActive)
                 .FirstOrDefaultAsync();
 
@@ -1034,7 +1034,7 @@ namespace CASNApp.API.Controllers
 
             callerEntity.UpdateFromModel(callerModel);
 
-            var appointmentEntity = await dbContext.Appointment
+            var appointmentEntity = await dbContext.Appointments
                 .Include(a => a.Drives)
                 .Where(a => a.Id == appointmentModel.Id && a.IsActive)
                 .FirstOrDefaultAsync();
@@ -1082,7 +1082,7 @@ namespace CASNApp.API.Controllers
                         Drive.DirectionToServiceProvider,
                         serviceProvider);
 
-                    dbContext.Drive.Add(driveToEntity);
+                    dbContext.Drives.Add(driveToEntity);
                 }
             }
             else
@@ -1115,7 +1115,7 @@ namespace CASNApp.API.Controllers
                         Drive.DirectionFromServiceProvider,
                         serviceProvider);
 
-                    dbContext.Drive.Add(driveFromEntity);
+                    dbContext.Drives.Add(driveFromEntity);
                 }
             }
             else

aspnetcore.api/CASNApp.API/Controllers/DriverApi.cs
@@ -81,14 +81,14 @@ namespace CASNApp.API.Controllers
                 return BadRequest(body);
             }
 
-            var drive = dbContext.Drive.Where(d => d.Id == driveId.Value && d.IsActive).SingleOrDefault();
+            var drive = dbContext.Drives.Where(d => d.Id == driveId.Value && d.IsActive).SingleOrDefault();
 
             if (drive == null)
             {
                 return NotFound(body);
             }
 
-            bool alreadyApplied = dbContext.VolunteerDriveLog
+            bool alreadyApplied = dbContext.VolunteerDriveLogs
                 .Include(vd => vd.Drive)
                 .Where(vd => vd.VolunteerId == volunteer.Id && vd.DriveId == drive.Id && vd.IsActive)
                 .Any();
@@ -107,7 +107,7 @@ namespace CASNApp.API.Controllers
                 DriveLogStatusId = Core.Entities.DriveLogStatus.APPLIED,
             };
 
-            dbContext.VolunteerDriveLog.Add(volunteerDriveLog);
+            dbContext.VolunteerDriveLogs.Add(volunteerDriveLog);
             
             if (drive.StatusId == Drive.StatusOpen)
             {
@@ -209,7 +209,7 @@ namespace CASNApp.API.Controllers
                 return BadRequest(body);
             }
 
-            var drive = await dbContext.Drive
+            var drive = await dbContext.Drives
                 .Where(d => d.Id == driveId.Value && d.IsActive)
                 .SingleOrDefaultAsync();
 
@@ -218,7 +218,7 @@ namespace CASNApp.API.Controllers
                 return NotFound(body);
             }
 
-            var volunteerDriveLogsForThisDrive = await dbContext.VolunteerDriveLog
+            var volunteerDriveLogsForThisDrive = await dbContext.VolunteerDriveLogs
                 .Where(vd => vd.DriveId == drive.Id &&
                     vd.IsActive &&
                     (vd.DriveLogStatusId == Core.Entities.DriveLogStatus.APPLIED ||
@@ -279,14 +279,14 @@ namespace CASNApp.API.Controllers
                     {
                         logger.LogInformation($"Checking badges for VolunteerDriveLog #{volunteerDriveLog.Id}");
 
-                        var volunteerBadges = await dbContext.VolunteerBadge
+                        var volunteerBadges = await dbContext.VolunteerBadges
                             .Where(vb => vb.VolunteerDriveLogId == volunteerDriveLog.Id)
                             .ToListAsync();
 
                         foreach (var volunteerBadge in volunteerBadges)
                         {
                             logger.LogInformation($"Removing VolunteerBadge #{volunteerBadge.Id} for badge #{volunteerBadge.BadgeId}");
-                            dbContext.VolunteerBadge.Remove(volunteerBadge);
+                            dbContext.VolunteerBadges.Remove(volunteerBadge);
                         }
                     }
 
@@ -336,7 +336,7 @@ namespace CASNApp.API.Controllers
                 end = end.Add(new TimeSpan(23, 59, 59));
             }
 
-            var appointmentEntities = await dbContext.Appointment
+            var appointmentEntities = await dbContext.Appointments
                 .AsNoTracking()
                 .Include(a => a.Caller)
                 .Include(a => a.Drives)

aspnetcore.api/CASNApp.Admin/Controllers/MessageErrorLogsController.cs
@@ -29,12 +29,12 @@ namespace CASNApp.Admin.Controllers
                 return Forbid();
             }
 
-            var allVolunteerNames = await _context.Volunteer
+            var allVolunteerNames = await _context.Volunteers
                 .AsNoTracking()
                 .Select(v => new { v.Id, Name = $"{v.FirstName} {v.LastName}" })
                 .ToDictionaryAsync(x => x.Id, x => x.Name);
 
-            var messageErrorLogs = await _context.MessageErrorLog
+            var messageErrorLogs = await _context.MessageErrorLogs
                 .AsNoTracking()
                 .OrderByDescending(mel => mel.DateSent)
                 .Take(50)
@@ -71,7 +71,7 @@ namespace CASNApp.Admin.Controllers
                 return NotFound();
             }
 
-            var messageErrorLog = await _context.MessageErrorLog
+            var messageErrorLog = await _context.MessageErrorLogs
                 .AsNoTracking()
                 .FirstOrDefaultAsync(m => m.Id == id);
 
@@ -97,7 +97,7 @@ namespace CASNApp.Admin.Controllers
 
             if (messageErrorLog.VolunteerId.HasValue)
             {
-                var volunteer = await _context.Volunteer
+                var volunteer = await _context.Volunteers
                     .AsNoTracking()
                     .FirstOrDefaultAsync(v => v.Id == messageErrorLog.VolunteerId.Value);
 

aspnetcore.api/CASNApp.Admin/Controllers/MessageLogsController.cs
@@ -28,7 +28,7 @@ namespace CASNApp.Admin.Controllers
                 return Forbid();
             }
 
-            return View(await _context.MessageLog
+            return View(await _context.MessageLogs
                 .OrderByDescending(ml => ml.Id)
                 .Take(50)
                 .OrderBy(ml => ml.Id)
@@ -48,7 +48,7 @@ namespace CASNApp.Admin.Controllers
                 return NotFound();
             }
 
-            var messageLog = await _context.MessageLog
+            var messageLog = await _context.MessageLogs
                 .FirstOrDefaultAsync(m => m.Id == id);
             if (messageLog == null)
             {

aspnetcore.api/CASNApp.Admin/Controllers/MessagesController.cs
@@ -29,7 +29,7 @@ namespace CASNApp.Admin.Controllers
                 return Forbid();
             }
 
-            var casn_appContext = _context.Message.Include(m => m.MessageType);
+            var casn_appContext = _context.Messages.Include(m => m.MessageType);
             return View(await casn_appContext.ToListAsync());
         }
 
@@ -46,7 +46,7 @@ namespace CASNApp.Admin.Controllers
                 return NotFound();
             }
 
-            var message = await _context.Message
+            var message = await _context.Messages
                 .Include(m => m.MessageType)
                 .FirstOrDefaultAsync(m => m.Id == id);
 
@@ -66,7 +66,7 @@ namespace CASNApp.Admin.Controllers
                 return Forbid();
             }
 
-            ViewData["MessageTypeId"] = new SelectList(_context.MessageType, "Id", "Name");
+            ViewData["MessageTypeId"] = new SelectList(_context.MessageTypes, "Id", "Name");
             return View();
         }
 
@@ -89,7 +89,7 @@ namespace CASNApp.Admin.Controllers
                 return RedirectToAction(nameof(Index));
             }
 
-            ViewData["MessageTypeId"] = new SelectList(_context.MessageType, "Id", "Name", message.MessageTypeId);
+            ViewData["MessageTypeId"] = new SelectList(_context.MessageTypes, "Id", "Name", message.MessageTypeId);
             return View(message);
         }
 
@@ -106,14 +106,14 @@ namespace CASNApp.Admin.Controllers
                 return NotFound();
             }
 
-            var message = await _context.Message.FindAsync(id);
+            var message = await _context.Messages.FindAsync(id);
 
             if (message == null)
             {
                 return NotFound();
             }
 
-            ViewData["MessageTypeId"] = new SelectList(_context.MessageType, "Id", "Name", message.MessageTypeId);
+            ViewData["MessageTypeId"] = new SelectList(_context.MessageTypes, "Id", "Name", message.MessageTypeId);
             return View(message);
         }
 
@@ -156,7 +156,7 @@ namespace CASNApp.Admin.Controllers
                 return RedirectToAction(nameof(Index));
             }
 
-            ViewData["MessageTypeId"] = new SelectList(_context.MessageType, "Id", "Name", message.MessageTypeId);
+            ViewData["MessageTypeId"] = new SelectList(_context.MessageTypes, "Id", "Name", message.MessageTypeId);
             return View(message);
         }
 
@@ -173,7 +173,7 @@ namespace CASNApp.Admin.Controllers
                 return NotFound();
             }
 
-            var message = await _context.Message
+            var message = await _context.Messages
                 .Include(m => m.MessageType)
                 .FirstOrDefaultAsync(m => m.Id == id);
 
@@ -195,8 +195,8 @@ namespace CASNApp.Admin.Controllers
                 return Forbid();
             }
 
-            var message = await _context.Message.FindAsync(id);
-            _context.Message.Remove(message);
+            var message = await _context.Messages.FindAsync(id);
+            _context.Messages.Remove(message);
             await _context.SaveChangesAsync();
 
             return RedirectToAction(nameof(Index));
@@ -204,7 +204,7 @@ namespace CASNApp.Admin.Controllers
 
         private bool MessageExists(int id)
         {
-            return _context.Message.Any(e => e.Id == id);
+            return _context.Messages.Any(e => e.Id == id);
         }
 
         private async Task<bool> UserHas2FA()

commit b0dfa36dac0bb0aa3a9592cc44719734b1e745e3

Resolved an issue introduced in commit 816d4d2

aspnetcore.api/CASNApp.Core/Entities/casn_appContext.cs
@@ -37,6 +37,8 @@ namespace CASNApp.Core.Entities
 
             modelBuilder.Entity<Appointment>(entity =>
             {
+                entity.ToTable("Appointment");
+
                 entity.HasIndex(e => e.AppointmentTypeId)
                     .HasName("FK_Appointment_AppointmentTypeId_idx");
 
@@ -136,6 +138,8 @@ namespace CASNApp.Core.Entities
 
             modelBuilder.Entity<AppointmentType>(entity =>
             {
+                entity.ToTable("AppointmentType");
+
                 entity.HasIndex(e => e.Name)
                     .HasName("UQ_AppointmentType_Name")
                     .IsUnique();
@@ -170,6 +174,8 @@ namespace CASNApp.Core.Entities
 
             modelBuilder.Entity<Badge>(entity =>
             {
+                entity.ToTable("Badge");
+
                 entity.Property(e => e.Id).ValueGeneratedNever();
 
                 entity.Property(e => e.Created)
@@ -220,6 +226,8 @@ namespace CASNApp.Core.Entities
 
             modelBuilder.Entity<Caller>(entity =>
             {
+                entity.ToTable("Caller");
+
                 entity.Property(e => e.CallerIdentifier)
                     .IsRequired()
                     .HasMaxLength(45);
@@ -256,6 +264,8 @@ namespace CASNApp.Core.Entities
 
             modelBuilder.Entity<Drive>(entity =>
             {
+                entity.ToTable("Drive");
+
                 entity.HasIndex(e => e.AppointmentId)
                     .HasName("FK_Drive_AppointmentId_idx");
 
@@ -350,6 +360,8 @@ namespace CASNApp.Core.Entities
 
             modelBuilder.Entity<DriveCancelReason>(entity =>
             {
+                entity.ToTable("DriveCancelReason");
+
                 entity.HasIndex(e => e.Name)
                     .HasName("UQ_DriveCancelReason_Name")
                     .IsUnique();
@@ -378,6 +390,8 @@ namespace CASNApp.Core.Entities
 
             modelBuilder.Entity<DriveStatus>(entity =>
             {
+                entity.ToTable("DriveStatus");
+
                 entity.HasIndex(e => e.Name)
                     .HasName("UQ_DriveStatus_Name")
                     .IsUnique();
@@ -404,6 +418,8 @@ namespace CASNApp.Core.Entities
 
             modelBuilder.Entity<Message>(entity =>
             {
+                entity.ToTable("Message");
+
                 entity.HasIndex(e => e.MessageTypeId)
                     .HasName("FK_Message_MessageTypeId_idx");
 
@@ -434,6 +450,8 @@ namespace CASNApp.Core.Entities
 
             modelBuilder.Entity<MessageErrorLog>(entity =>
             {
+                entity.ToTable("MessageErrorLog");
+
                 entity.HasIndex(e => e.DateSent);
 
                 entity.Property(e => e.AppointmentId);
@@ -470,6 +488,8 @@ namespace CASNApp.Core.Entities
 
             modelBuilder.Entity<MessageLog>(entity =>
             {
+                entity.ToTable("MessageLog");
+
                 entity.HasIndex(e => e.DateSent);
 
 				entity.Property(e => e.AppointmentId);
@@ -500,6 +520,8 @@ namespace CASNApp.Core.Entities
 
             modelBuilder.Entity<MessageType>(entity =>
             {
+                entity.ToTable("MessageType");
+
                 entity.HasIndex(e => e.Name)
                     .HasName("UQ_MessageType_Name")
                     .IsUnique();
@@ -527,6 +549,8 @@ namespace CASNApp.Core.Entities
 
             modelBuilder.Entity<ServiceProvider>(entity =>
             {
+                entity.ToTable("ServiceProvider");
+
                 entity.HasIndex(e => e.ServiceProviderTypeId)
                     .HasName("FK_ServiceProvider_ServiceProviderTypeId_idx");
 
@@ -584,6 +608,8 @@ namespace CASNApp.Core.Entities
 
             modelBuilder.Entity<ServiceProviderType>(entity =>
             {
+                entity.ToTable("ServiceProviderType");
+
                 entity.HasIndex(e => e.Name)
                     .HasName("UQ_ServiceProviderType_Name")
                     .IsUnique();
@@ -611,6 +637,8 @@ namespace CASNApp.Core.Entities
 
             modelBuilder.Entity<Volunteer>(entity =>
             {
+                entity.ToTable("Volunteer");
+
                 entity.Property(e => e.Address).HasMaxLength(100);
 
                 entity.Property(e => e.City).HasMaxLength(50);