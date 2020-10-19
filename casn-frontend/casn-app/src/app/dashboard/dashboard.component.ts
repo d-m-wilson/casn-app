1643ccb03ee917de71a64e48eb787c1ef5167214
Aspen Hollyer <a.hollyer@cpanel.net>
2020-10-19 15:56:08 -0500

    Remove unused import

--- casn-frontend/casn-app/src/app/dashboard/dashboard.component.ts
+++ casn-frontend/casn-app/src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, RoutesRecognized } from '@angular/router';
-import { filter, pairwise } from 'rxjs/operators';
 import { Constants } from '../app.constants';
 
 @Component({
