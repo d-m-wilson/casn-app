29c4b7b8059e5504a321b1c06a600be5ceacbf40
Aspen Hollyer <a.hollyer@cpanel.net>
2020-10-30 12:54:54 -0500

    Clean up, reorganize file structure

--- casn-frontend/casn-app/src/app/app.module.ts
+++ casn-frontend/casn-app/src/app/app.module.ts
@@ -6,31 +6,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgxMaskModule } from 'ngx-mask';
 import { OwlDateTimeModule, OwlNativeDateTimeModule } from '@danielmoncada/angular-datetime-picker';
-import { AgmCoreModule, LAZY_MAPS_API_CONFIG, MapsAPILoader } from '@agm/core';
+import { AgmCoreModule, MapsAPILoader } from '@agm/core';
 import { AgmSnazzyInfoWindowModule } from '@agm/snazzy-info-window';
 import { DatePipe } from '@angular/common';
-/* Angular Material Components */
-import { MatAutocompleteModule } from '@angular/material/autocomplete';
-import { MatBadgeModule } from '@angular/material/badge';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatNativeDateModule } from '@angular/material/core';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatExpansionModule } from '@angular/material/expansion';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
-import { MatListModule } from '@angular/material/list';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatRadioModule } from '@angular/material/radio';
-import { MatSelectModule } from '@angular/material/select';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { MatStepperModule } from '@angular/material/stepper';
-import { MatTableModule } from '@angular/material/table';
-import { MatTabsModule } from '@angular/material/tabs';
-import { MatToolbarModule } from '@angular/material/toolbar';
+import { SharedModule } from './shared/shared.module';
 /* Routing */
 import { AppRoutingModule } from './app-routing.module';
 /* Custom Services & HTTP Interceptors */
@@ -44,10 +23,7 @@ import { ErrorInterceptor } from './auth-services/error.interceptor';
 import { UserService } from './auth-services/user.service';
 import { ApiModule } from './api/api.module';
 import { ServiceWorkerModule } from '@angular/service-worker';
-/* Custom Directives & Pipes */
-import { MatVerticalStepperScrollerDirective } from './directives/mat-vertical-stepper.directive';
 /* Custom Components */
-import { SharedModule } from './shared/shared.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
 import { CallersComponent } from './callers/callers.component';
@@ -67,7 +43,6 @@ import { RideshareModalComponent } from './rideshare-modal/rideshare-modal.compo
 
 @NgModule({
   declarations: [
-    MatVerticalStepperScrollerDirective,
     AppComponent,
     DashboardComponent,
     CallersComponent,
@@ -98,27 +73,6 @@ import { RideshareModalComponent } from './rideshare-modal/rideshare-modal.compo
     HttpClientModule,
     ReactiveFormsModule,
     AppRoutingModule,
-    MatAutocompleteModule,
-    MatBadgeModule,
-    MatButtonModule,
-    MatCardModule,
-    MatCheckboxModule,
-    MatDatepickerModule,
-    MatExpansionModule,
-    MatFormFieldModule,
-    MatIconModule,
-    MatInputModule,
-    MatListModule,
-    MatMenuModule,
-    MatNativeDateModule,
-    MatRadioModule,
-    MatSelectModule,
-    MatSidenavModule,
-    MatSlideToggleModule,
-    MatStepperModule,
-    MatTableModule,
-    MatTabsModule,
-    MatToolbarModule,
     ServiceWorkerModule.register('./ngsw-worker.js', { enabled: environment.production }),
     HammerModule,
     SharedModule, // TODO: Remove once modules are broken up
@@ -133,7 +87,6 @@ import { RideshareModalComponent } from './rideshare-modal/rideshare-modal.compo
     UserService,
     { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
     { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
-    // { provide: LAZY_MAPS_API_CONFIG, useClass: GoogleMapConfigService },
     { provide: MapsAPILoader, useClass: GoogleMapConfigService },
     DatePipe
   ],
--- casn-frontend/casn-app/src/app/shared/shared.module.ts
+++ casn-frontend/casn-app/src/app/shared/shared.module.ts
@@ -1,16 +1,40 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RideCardComponent } from './casn-ui/ride-card/ride-card.component';
-import { MatExpansionModule } from '@angular/material/expansion';
+
+/* Angular Material */
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatBadgeModule } from '@angular/material/badge';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
-import { RideCTAPipe } from '../pipes/ride-cta.pipe';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatTableModule } from '@angular/material/table';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+import { RideCardComponent } from './casn-ui/ride-card/ride-card.component';
+import { RideCTAPipe } from './pipes/ride-cta.pipe';
 import { RidesHeaderComponent } from './casn-ui/rides-header/rides-header.component';
-import { SafeUrlPipe } from '../pipes/safe-url.pipe';
-import { PhonePipe } from '../pipes/phone.pipe';
+import { SafeUrlPipe } from './pipes/safe-url.pipe';
+import { PhonePipe } from './pipes/phone.pipe';
+import { MatVerticalStepperScrollerDirective } from './directives/mat-vertical-stepper.directive';
+
 
 @NgModule({
   declarations: [
+    MatVerticalStepperScrollerDirective,
     SafeUrlPipe,
     PhonePipe,
     RideCardComponent,
@@ -19,15 +43,60 @@ import { PhonePipe } from '../pipes/phone.pipe';
   ],
   imports: [
     CommonModule,
-    MatExpansionModule,
-    MatIconModule,
+
+    /* Angular Material */
+    MatAutocompleteModule,
+    MatBadgeModule,
     MatButtonModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatDatepickerModule,
+    MatExpansionModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatMenuModule,
+    MatNativeDateModule,
+    MatRadioModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatSlideToggleModule,
+    MatStepperModule,
+    MatTableModule,
+    MatTabsModule,
+    MatToolbarModule,
   ],
   exports: [
+    MatVerticalStepperScrollerDirective,
     SafeUrlPipe,
     PhonePipe,
+    RideCTAPipe,
     RideCardComponent,
     RidesHeaderComponent,
+
+    /* Angular Material */
+    MatAutocompleteModule,
+    MatBadgeModule,
+    MatButtonModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatDatepickerModule,
+    MatExpansionModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatMenuModule,
+    MatNativeDateModule,
+    MatRadioModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatSlideToggleModule,
+    MatStepperModule,
+    MatTableModule,
+    MatTabsModule,
+    MatToolbarModule,
   ]
 })
 export class SharedModule { }
