c74bcea09cc2d6848d743f4491c797f2e0884133
David M. Wilson <david.maurice.wilson@gmail.com>
2020-12-28 10:45:09 -0600

    previous commit put the constants into the wrong source file; moved them into FundingOfferStatusPartial.cs

--- aspnetcore.api/CASNApp.Core/Entities/FundingOfferStatusPartial.cs
+++ aspnetcore.api/CASNApp.Core/Entities/FundingOfferStatusPartial.cs
@@ -4,5 +4,11 @@ namespace CASNApp.Core.Entities
 {
     public partial class FundingOfferStatus : ISoftDelete, ICreatedDate, IUpdatedDate
     {
+        public const int Draft = 1;
+        public const int Issued = 2;
+        public const int Redeemed = 3;
+        public const int Paid = 4;
+        public const int Voided = 5;
+
     }
 }
