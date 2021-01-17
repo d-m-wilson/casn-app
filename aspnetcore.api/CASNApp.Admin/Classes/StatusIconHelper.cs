931fb8479ef4d41b78066408d27290f6aaf9f929
David M. Wilson <david.maurice.wilson@gmail.com>
2021-01-17 17:34:33 -0600

    added status icons to /FundingOffers/Index

---
+++ aspnetcore.api/CASNApp.Admin/Classes/StatusIconHelper.cs
@@ -0,0 +1,46 @@
+﻿using CASNApp.Core.Entities;
+
+namespace CASNApp.Admin.Classes
+{
+    public static class StatusIconHelper
+    {
+        public static string GetHtml(int statusId)
+        {
+            string statusName;
+            string iconName;
+
+            switch (statusId)
+            {
+                case FundingOfferStatus.Draft:
+                    statusName = nameof(FundingOfferStatus.Draft);
+                    iconName = "file-earmark-medical";
+                    break;
+                case FundingOfferStatus.Issued:
+                    statusName = nameof(FundingOfferStatus.Issued);
+                    iconName = "file-earmark-text";
+                    break;
+                case FundingOfferStatus.Redeemed:
+                    statusName = nameof(FundingOfferStatus.Redeemed);
+                    iconName = "check2-circle";
+                    break;
+                case FundingOfferStatus.Paid:
+                    statusName = nameof(FundingOfferStatus.Paid);
+                    iconName = "receipt-cutoff";
+                    break;
+                case FundingOfferStatus.Voided:
+                    statusName = nameof(FundingOfferStatus.Voided);
+                    iconName = "x-circle";
+                    break;
+                default:
+                    return "";
+            }
+
+            statusName = statusName.ToLowerInvariant();
+
+            var result = $"<i class=\"bi-{iconName} status-{statusName}\"></i>";
+
+            return result;
+        }
+
+    }
+}
